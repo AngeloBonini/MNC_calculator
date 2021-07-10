f56f6719ceb8dacf2d4363ab6fae3f4d40048417
Angelo Bonini <abonini2000@gmail.com>
2021-07-10 19:59:17 -0300

    creating a new lib

--- trabalho1/methods_calculator.c
+++ trabalho1/methods_calculator.c
@@ -1,56 +1,7 @@
 #include "read_print.h"
-#include "calc.h"
 #include <math.h>
 #include <stdbool.h>
 
-// void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX])
-// {
-//    for (int i = 1; i < n; i++)
-//       for (int j = 0, k = 0; j < n; j++, k++)
-//       {
-//          if (j != c)
-//             sub[i - 1][k] = a[i][j];
-//          else
-//             k--;
-//       }
-// }
-
-// void transpoe(int n, double a[][MAX])
-// {
-//    double aux;
-//    for (int i = 0; i < n; i++)
-//       for (int j = i + 1; j < n; j++)
-//       {
-//          aux = a[i][j];
-//          a[i][j] = a[j][i];
-//          a[j][i] = aux;
-//       }
-// }
-
-// void id(int n, double a[][MAX])
-// {
-//    for (int i = 0; i < n; i++)
-//       for (int j = 0; j < n; j++)
-//          a[i][j] = (i == j) ? 1 : 0;
-// }
-
-// double determinante(int n, double a[][MAX])
-// {
-//    double s = 0;
-//    if (n == 1)
-//       return a[0][0];
-//    else
-//    {
-//       double sub[MAX][MAX];
-//       for (int j = 0; j < n; j++)
-//          if (a[0][j] != 0)
-//          {
-//             geraSubMatriz(n, a, j, sub);
-//             s += a[0][j] * pow(-1, j) * determinante(n - 1, sub);
-//          }
-//    }
-//    return s;
-// }
 
 bool temSubMatrizesNaoSingulares(int n, double a[][MAX])
 {
