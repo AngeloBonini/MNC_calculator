73ecf77171a856713b72811d8a9e4c59045924a0
Angelo Bonini <abonini2000@gmail.com>
2021-07-10 19:22:06 -0300

    remaning some functions

--- trabalho1/methods_calculator.c
+++ trabalho1/methods_calculator.c
@@ -1,14 +1,17 @@
-#include "header.h"
+#include "read_print.h"
 #include <math.h>
+#include <stdbool.h>
 
 //a[][]: coeficientes
 //b[]  : termos independentes
 //x[]  : solução
 
 //gera matriz sub de ordem n-1, sem a linha 0 e a coluna c da matriz a
-void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX]) {
+void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX])
+{
    for (int i = 1; i < n; i++)
-      for (int j = 0, k = 0; j < n; j++, k++) {
+      for (int j = 0, k = 0; j < n; j++, k++)
+      {
          if (j != c)
             sub[i - 1][k] = a[i][j];
          else
@@ -16,30 +19,36 @@ void geraSubMatriz(int n, double a[][MAX], int c, double sub[][MAX]) {
       }
 }
 //reescreve a como sua transposta
-void transpoe(int n, double a[][MAX]) {
+void transpoe(int n, double a[][MAX])
+{
    double aux;
    for (int i = 0; i < n; i++)
-      for (int j = i + 1; j < n; j++) {
+      for (int j = i + 1; j < n; j++)
+      {
          aux = a[i][j];
          a[i][j] = a[j][i];
          a[j][i] = aux;
       }
 }
 //reescreve a como a identidade de ordem n
-void id(int n, double a[][MAX]) {
+void id(int n, double a[][MAX])
+{
    for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
          a[i][j] = (i == j) ? 1 : 0;
 }
 //retorna o determinante da matriz, por laplace
-double determinante(int n, double a[][MAX]) {
+double determinante(int n, double a[][MAX])
+{
    double s = 0;
    if (n == 1)
       return a[0][0];
-   else {
+   else
+   {
       double sub[MAX][MAX];
       for (int j = 0; j < n; j++)
-         if (a[0][j] != 0) { //para cada elemento da linha 0 (a[0][j])
+         if (a[0][j] != 0)
+         { //para cada elemento da linha 0 (a[0][j])
             geraSubMatriz(n, a, j, sub);
             s += a[0][j] * pow(-1, j) * determinante(n - 1, sub);
          }
@@ -47,59 +56,69 @@ double determinante(int n, double a[][MAX]) {
    return s;
 }
 //retorna true se todas as submatrizes principais têm determinante diferente de 0
-bool temSubMatrizesNaoSingulares(int n, double a[][MAX]) {
+bool temSubMatrizesNaoSingulares(int n, double a[][MAX])
+{
    for (int i = 1; i <= n; i++)
       if (determinante(i, a) == 0)
          return false;
    return true;
 }
 //retorna true se todas as submatrizes principais têm determinante maior que 0
-bool ehDefPositiva(int n, double a[][MAX]) {
+bool ehDefPositiva(int n, double a[][MAX])
+{
    for (int i = 1; i <= n; i++)
       if (determinante(i, a) <= 0)
          return false;
    return true;
 }
-bool ehSimetrica(int n, double a[][MAX]) {
+bool ehSimetrica(int n, double a[][MAX])
+{
    for (int i = 0; i < n; i++)
       for (int j = i + 1; j < n; j++)
          if (a[i][j] != a[j][i])
             return false;
    return true;
 }
-void copiaMatriz(int n, double a[][MAX], double copia[][MAX]) {
+void copiaMatriz(int n, double a[][MAX], double copia[][MAX])
+{
    for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
          copia[i][j] = a[i][j];
 }
-void copiaVet(int n, double a[], double copia[]) {
+void copiaVet(int n, double a[], double copia[])
+{
    for (int i = 0; i < n; i++)
       copia[i] = a[i];
 }
-bool diagPrincipalNaoNula(int n, double a[][MAX]) {
+bool diagPrincipalNaoNula(int n, double a[][MAX])
+{
    for (int i = 0; i < n; i++)
       if (a[i][i] == 0)
          return false;
    return true;
 }
-bool criterioLinhas(int n, double a[][MAX]) {
+bool criterioLinhas(int n, double a[][MAX])
+{
    for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
          if (j != i && abs(a[i][j] / a[i][i]) >= 1)
             return false;
    return true;
 }
-bool criterioColunas(int n, double a[][MAX]) {
+bool criterioColunas(int n, double a[][MAX])
+{
    for (int j = 0; j < n; j++)
       for (int i = 0; i < n; i++)
          if (i != j && abs(a[i][j] / a[j][j]) >= 1)
             return false;
    return true;
 }
-bool criterioSassenfeld(int n, double a[][MAX]) {
+bool criterioSassenfeld(int n, double a[][MAX])
+{
    double beta[MAX], max = 0;
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; i++)
+   {
       beta[i] = 0;
       for (int j = 0; j < i - 1; j++)
          beta[i] += abs(a[i][j] / a[i][i]) * beta[j];
@@ -113,7 +132,8 @@ bool criterioSassenfeld(int n, double a[][MAX]) {
    return true;
 }
 //retorna a norma infinita do vetor
-double normaInf(int n, double v[]) {
+double normaInf(int n, double v[])
+{
    double max = 0;
    for (int i = 0; i < n; i++)
       if (abs(v[i]) > max)
@@ -121,18 +141,21 @@ double normaInf(int n, double v[]) {
    return max;
 }
 //retorna um vetor de ordem n correspondente a v1-v2 (elemento a elemento)
-double *diferencaVet(int n, double v1[], double v2[]) {
+double *diferencaVet(int n, double v1[], double v2[])
+{
    double *v = (double *)malloc(sizeof(double) * n);
    for (int i = 0; i < n; i++)
       v[i] = v1[i] - v2[i];
    return v;
 }
 
-bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[]) {
+bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[])
+{
    if (determinante(n, a) == 0)
       return false;
 
-   for (int i = n - 1; i >= 0; i--) {
+   for (int i = n - 1; i >= 0; i--)
+   {
       double s = 0;
       for (int j = i + 1; j < n; j++)
          s += a[i][j] * x[j];
@@ -141,11 +164,13 @@ bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[]) {
    }
    return true;
 }
-bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[]) {
+bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[])
+{
    if (determinante(n, a) == 0)
       return false;
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; i++)
+   {
       double s = 0;
       for (int j = 0; j < i; j++)
          s += a[i][j] * x[j];
@@ -155,15 +180,18 @@ bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[]) {
    return true;
 }
 //métodos diretos
-bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
+bool decomposicaoLU(int n, double a[][MAX], double b[], double x[])
+{
    double u[MAX][MAX], l[MAX][MAX], y[MAX], s;
 
    if (!temSubMatrizesNaoSingulares(n, a))
       return false; //método não converge
 
-   for (int p = 0; p < n; p++) {
+   for (int p = 0; p < n; p++)
+   {
       //calcula a linha p de U
-      for (int j = p; j < n; j++) {
+      for (int j = p; j < n; j++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[p][k] * u[k][j];
@@ -171,7 +199,8 @@ bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
          u[p][j] = a[p][j] - s;
       }
       //calcula a coluna p de L
-      for (int i = p; i < n; i++) {
+      for (int i = p; i < n; i++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[i][k] * u[k][p];
@@ -185,15 +214,18 @@ bool decomposicaoLU(int n, double a[][MAX], double b[], double x[]) {
    sistemaTriangularSuperior(n, u, y, x); //U.x = y
    return true;
 }
-bool gaussCompacto(int n, double a[][MAX], double b[], double x[]) {
+bool gaussCompacto(int n, double a[][MAX], double b[], double x[])
+{
    double u[MAX][MAX], l[MAX][MAX], bL[MAX], s;
 
    if (!temSubMatrizesNaoSingulares(n, a))
       return false; //método não converge
 
-   for (int p = 0; p < n; p++) {
+   for (int p = 0; p < n; p++)
+   {
       //calcula a linha p de U
-      for (int j = p; j < n; j++) {
+      for (int j = p; j < n; j++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[p][k] * u[k][j];
@@ -210,7 +242,8 @@ bool gaussCompacto(int n, double a[][MAX], double b[], double x[]) {
       bL[p] = b[p] - s;
 
       //calcula a coluna p de L
-      for (int i = p; i < n; i++) {
+      for (int i = p; i < n; i++)
+      {
          s = 0;
          for (int k = 0; k < p; k++)
             s += l[i][k] * u[k][p];
@@ -224,13 +257,15 @@ bool gaussCompacto(int n, double a[][MAX], double b[], double x[]) {
    sistemaTriangularSuperior(n, u, bL, x); //U.x = bL
    return true;
 }
-bool cholesky(int n, double a[][MAX], double b[], double x[]) {
+bool cholesky(int n, double a[][MAX], double b[], double x[])
+{
    double l[MAX][MAX], y[MAX], s;
 
    if (!ehSimetrica(n, a) || !ehDefPositiva(n, a))
       return false; //método não converge
 
-   for (int j = 0; j < n; j++) {
+   for (int j = 0; j < n; j++)
+   {
       //calcula o elemento da diagonal
       s = 0;
       for (int k = 0; k < j; k++)
@@ -239,7 +274,8 @@ bool cholesky(int n, double a[][MAX], double b[], double x[]) {
       l[j][j] = sqrt(a[j][j] - s);
 
       //calcula os elementos abaixo da diagonal
-      for (int i = j + 1; i < n; i++) {
+      for (int i = j + 1; i < n; i++)
+      {
          s = 0;
          for (int k = 0; k < j; k++)
             s += l[i][k] * l[j][k];
@@ -254,7 +290,8 @@ bool cholesky(int n, double a[][MAX], double b[], double x[]) {
    sistemaTriangularSuperior(n, l, y, x);
    return true;
 }
-bool gaussJordan(int n, double a[][MAX], double b[], double x[]) {
+bool gaussJordan(int n, double a[][MAX], double b[], double x[])
+{
    double aL[MAX][MAX], bL[MAX], m;
    copiaMatriz(n, a, aL);
    copiaVet(n, b, bL);
@@ -262,9 +299,11 @@ bool gaussJordan(int n, double a[][MAX], double b[], double x[]) {
    if (!temSubMatrizesNaoSingulares(n, a))
       return false; //método não converge
 
-   for (int k = 0; k < n; k++) {
+   for (int k = 0; k < n; k++)
+   {
       for (int i = 0; i < n; i++)
-         if (i != k) { //para cada linha i diferente da linha do pivô (k)
+         if (i != k)
+         { //para cada linha i diferente da linha do pivô (k)
             //razão entre o elemento que deve ser zerado e o pivô
             m = aL[i][k] / aL[k][k];
 
@@ -283,12 +322,14 @@ bool gaussJordan(int n, double a[][MAX], double b[], double x[]) {
       x[i] = bL[i] / aL[i][i];
    return true;
 }
-bool matrizInversa(int n, double a[][MAX], double x[][MAX]) {
+bool matrizInversa(int n, double a[][MAX], double x[][MAX])
+{
    int op;
    double e[MAX][MAX];
 
    printf("\n[0] Decomposição LU\n[1] Gauss Compacto\n");
-   do {
+   do
+   {
       printf("Determinar a inversa por: ");
       scanf("%d", &op);
    } while (op != 0 && op != 1);
@@ -306,13 +347,16 @@ bool matrizInversa(int n, double a[][MAX], double x[][MAX]) {
 }
 
 //métodos iterativos
-bool jacobi(int n, double a[][MAX], double b[], double e, double x_ant[], int maxIte, double x[], int *ite) {
+bool jacobi(int n, double a[][MAX], double b[], double e, double x_ant[], int maxIte, double x[], int *ite)
+{
    if (!diagPrincipalNaoNula(n, a) || determinante(n, a) == 0 || (!criterioLinhas(n, a) && !criterioColunas(n, a)))
       return false; //método não converge
 
    double *v, s;
-   for (*ite = 1; *ite <= maxIte; (*ite)++) {
-      for (int i = 0; i < n; i++) {
+   for (*ite = 1; *ite <= maxIte; (*ite)++)
+   {
+      for (int i = 0; i < n; i++)
+      {
          s = 0;
          for (int j = 0; j < n; j++)
             if (j != i)
@@ -330,15 +374,18 @@ bool jacobi(int n, double a[][MAX], double b[], double e, double x_ant[], int ma
    (*ite)--; //estourou número máximo de iterações
    return true;
 }
-bool gaussSeidel(int n, double a[][MAX], double b[], double e, double x_ant[], int maxIte, double x[], int *ite) {
+bool gaussSeidel(int n, double a[][MAX], double b[], double e, double x_ant[], int maxIte, double x[], int *ite)
+{
    if (!diagPrincipalNaoNula(n, a) || determinante(n, a) == 0 || (!criterioLinhas(n, a) && !criterioSassenfeld(n, a)))
       return false; //método não converge
 
    double *v, s, x_rec[MAX];
 
    copiaVet(n, x_ant, x_rec);
-   for (*ite = 1; *ite <= maxIte; (*ite)++) {
-      for (int i = 0; i < n; i++) {
+   for (*ite = 1; *ite <= maxIte; (*ite)++)
+   {
+      for (int i = 0; i < n; i++)
+      {
          s = 0;
          for (int j = 0; j < n; j++)
             if (j != i)
@@ -358,7 +405,8 @@ bool gaussSeidel(int n, double a[][MAX], double b[], double e, double x_ant[], i
    return true;
 }
 
-int menu() {
+int menu()
+{
    int op;
    printf("   Cálculo de sistemas lineares   \n");
    printf("         e matriz inversa         \n");
@@ -378,7 +426,8 @@ int menu() {
    printf("     09 › Gauss Seidel\n\n");
    printf("| Matriz:\n");
    printf("  10 › Inversa\n\n");
-   do {
+   do
+   {
       printf("Resp.: ");
       scanf("%d", &op);
    } while (op < 1 || op > 10);
@@ -386,28 +435,32 @@ int menu() {
    return op;
 }
 
-int main() {
+int main()
+{
    int op, n, maxIte, ite;
    double a[MAX][MAX], i[MAX][MAX], x[MAX], x0[MAX], b[MAX], e;
    char r;
    bool ok;
 
-   do {
+   do
+   {
       system("clear");
       op = menu();
 
       printf("Ordem da matriz: ");
       scanf("%d", &n);
       printf("\nCoeficientes da matriz %dx%d:\n", n, n);
-      leMatriz(n, a);
+      read2d_array(n, a);
 
       if (op == 1)
          printf("\nA solução é: %.4lf\n", determinante(n, a));
-      else if (op >= 2 && op <= 7) {
+      else if (op >= 2 && op <= 7)
+      {
          printf("\nTermos independentes:\n");
-         leVetor(n, b);
+         readArray(n, b);
 
-         switch (op) {
+         switch (op)
+         {
          case 2:
             ok = sistemaTriangularInferior(n, a, b, x);
             break;
@@ -427,16 +480,20 @@ int main() {
             ok = gaussJordan(n, a, b, x);
             break;
          }
-         if (ok) {
+         if (ok)
+         {
             printf("\nO vetor solução é:\n");
-            impVetor(n, x);
-         } else
+            printArray(n, x);
+         }
+         else
             printf("\nO método não converge!\n");
-      } else if (op == 8 || op == 9) {
+      }
+      else if (op == 8 || op == 9)
+      {
          printf("\nTermos independentes:\n");
-         leVetor(n, b);
+         readArray(n, b);
          printf("\nAproximação inicial para o vetor solução:\n");
-         leVetor(n, x0);
+         readArray(n, x0);
          printf("\nErro: ");
          scanf("%lf", &e);
          printf("\nNúmero máximo de iterações: ");
@@ -447,21 +504,28 @@ int main() {
          else
             ok = gaussSeidel(n, a, b, e, x0, maxIte, x, &ite);
 
-         if (ok) {
+         if (ok)
+         {
             printf("\nO vetor solução é:\n");
-            impVetor(n, x);
-         } else
+            printArray(n, x);
+         }
+         else
             printf("\nO método não converge!\n");
-      } else {
+      }
+      else
+      {
          ok = matrizInversa(n, a, i);
-         if (ok) {
+         if (ok)
+         {
             printf("A matriz solução é:\n");
-            impMatriz(n, i);
-         } else
+            print2d_array(n, i);
+         }
+         else
             printf("\nO método não converge!\n");
       }
 
-      do {
+      do
+      {
          printf("\nRetornar ao menu [s/n]? ");
          fflush(stdin);
          scanf(" %c", &r);
