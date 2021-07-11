fab0ec3d1b7c3b37acdbb4ac354d807f85483184
Angelo Bonini <abonini2000@gmail.com>
2021-07-10 22:22:57 -0300

    biblioteca para o sistema triangular superior

--- trabalho1/methods_calculator.c
+++ trabalho1/methods_calculator.c
@@ -4,6 +4,8 @@
 #include "matriz.h"
 #include "testes_matrizes.h"
 #include "norma_inferior.h"
+#include "sistemaTriangularSuperior.h"
+#include "sistemaTriangularInferior.h"
 
 
 
@@ -52,36 +54,8 @@ double *diferencaVet(int n, double v1[], double v2[])
    return v;
 }
 
-bool sistemaTriangularSuperior(int n, double a[][MAX], double b[], double x[])
-{
-   if (determinante(n, a) == 0)
-      return false;
 
-   for (int i = n - 1; i >= 0; i--)
-   {
-      double s = 0;
-      for (int j = i + 1; j < n; j++)
-         s += a[i][j] * x[j];
 
-      x[i] = (b[i] - s) / a[i][i];
-   }
-   return true;
-}
-bool sistemaTriangularInferior(int n, double a[][MAX], double b[], double x[])
-{
-   if (determinante(n, a) == 0)
-      return false;
-
-   for (int i = 0; i < n; i++)
-   {
-      double s = 0;
-      for (int j = 0; j < i; j++)
-         s += a[i][j] * x[j];
-
-      x[i] = (b[i] - s) / a[i][i];
-   }
-   return true;
-}
 
 bool decomposicaoLU(int n, double a[][MAX], double b[], double x[])
 {
